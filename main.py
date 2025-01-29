74fb34a6c631ad48970aa03dc681fc9053647c3f
giovacast <giovacastiglione@gmail.com>
2025-01-29 17:27:03 -0400

    Added function to main.py that counts how many of each character does the text contain

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ def main():
         for word in words:
             word_count += 1
         return word_count
+    
+    def count_characters():
+        character_count = {}
+        for i in file_contents.lower():
+            if i not in character_count:
+                character_count[i] = 0
+            if i in character_count:
+                character_count[i] += 1
+        return character_count
 
+    print(count_characters())
     print(count_words())
 main()
