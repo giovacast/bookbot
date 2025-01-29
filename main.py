2404623169ed18ff7af7865ce1c1079cf3c62775
giovacast <giovacastiglione@gmail.com>
2025-01-29 16:22:32 -0400

    moved code to read the file to outside of the count_words function so it can be used by other functions

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
-    def count_words(text):
+    with open("books/frankenstein.txt") as file:
+        file_contents = file.read()
+    def count_words():
         word_count = 0
-        with open(text) as file:
-            file_contents = file.read()
-            words = file_contents.split()
-            for word in words:
-                word_count += 1
+        words = file_contents.split()
+        for word in words:
+            word_count += 1
         return word_count
 
-    print(count_words("books/frankenstein.txt"))
+    print(count_words())
 main()
