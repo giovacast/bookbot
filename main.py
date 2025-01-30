fd9489e3a91897a5ec4f8d08a3817ec7063d97bc
giovacast <giovacastiglione@gmail.com>
2025-01-30 07:12:37 -0400

    Added function to main.py that generates a report showing the total words in the text and how many of each letter it contains

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ def main():
                 character_count[i] += 1
         return character_count
 
-    print(count_characters())
-    print(count_words())
+    def generate_report():
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+                    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                    'y', 'z']
+        print("--- Begin report of books/frankenstein.txt ---\n"
+              f"{count_words()} words found in the document \n")
+        for k in count_characters():
+            if k in alphabet:
+                value = count_characters()[k]
+                print(f"The '{k}' character was found {value} times")
+        print("--- End report ---")
+
+    generate_report()
 main()
